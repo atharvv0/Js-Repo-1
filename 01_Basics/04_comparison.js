872aff435b4c9a4351c47caacf5bf0d4d84a385c
Atharva Kshirsagar <atharva0604@hotmail.com>
2025-08-26 10:49:23 +0000

    comparison operators and sp. cases.

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(2>3) // false  = boolean output
+
+// more comparison operators <,>,==,<=,>=,!=
+
+// special cases (Avoid)
+
+console.log("2"<3)  // true // string no. is converted into number
+console.log(null < 0)
+console.log(null > 0)
+console.log(null == 0)
+
+console.log(undefined < 0)
+console.log(undefined > 0)
+console.log(undefined == 0)
+
+// === Strictly check
+
+console.log("2" === 2) // strictly checked, strict comparison and not conversion
