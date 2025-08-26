a33b899db0172db5b11a62b08aede1824b593958
Atharva Kshirsagar <atharva0604@hotmail.com>
2025-08-26 10:38:59 +0000

    operations and operators.

--- 01_Basics/03_conversionoperations.js
+++ 01_Basics/03_conversionoperations.js
@@ -1,4 +1,4 @@
-let score = 369
+/*let score = 369
 let score2 = "777"
 let score3 = "27abc"
 
@@ -24,6 +24,45 @@ console.log(someNumber)
 console.log(typeof someNumber)
 
 // "369" = 369 easily converted into number
-// "367abc" = NaN (Not a Number) 
+// "367abc" = NaN (Not a Number)
 // boolean = 1==true , 2==false
 // boolean to string = "Atharva" = true
+*/
+
+// -----------------------------------Operations-----------------------------------------
+
+let value = 3
+let ngtvalue = -value
+
+console.log(ngtvalue);
+
+let str1 = "hello"
+let str2 = "Atharva"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+// Some Unique Cases
+
+console.log(3+"5") // 35
+console.log("3"+5)  //35
+console.log(3+5+ "2")   // 82
+console.log((3+4) * 3) // 21             // maintain readability
+console.log("2"+ 2+3) // 223
+
+console.log(true) //true
+console.log(+true) // 1                   // not preffered
+console.log(+"")
+
+
+
+let counter = 665
+++counter;                 // prefix increment = value is incremented first and then returned
+console.log(counter)
+
+let counter2 = 665
+counter2++;                   // postfix increment = value is returened before incrementing
+console.log(counter)
+
+
