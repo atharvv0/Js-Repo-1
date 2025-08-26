5816769215c5f01543930c35f100ba7e5968ae9f
Atharva Kshirsagar <atharva0604@hotmail.com>
2025-08-26 09:48:29 +0000

    JavaScripts datatypes and cases.

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat the code as the newer version of the Js
+
+// alert(3+3); // we are using nodejs and not browser
+
+console.log(3 + 3);
+console.log("hello World"); // maintain code readability
+
+let name = "Atharva" // String Datatype
+let age = 18
+let isLoggedIn = false
+
+// datatypes :
+// number --> 2^53 nos.
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined
+// symbol = unique
+// objects
+
+console.log(typeof "Atharva") // type = string
+
+// unique cases 
+
+console.log(typeof null);         // object
+console.log(typeof undefined);    // undefined
