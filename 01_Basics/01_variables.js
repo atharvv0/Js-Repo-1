0cb3cae121b4dd241362bbc4415fec3d0fe5f9f6
Atharva Kshirsagar <atharva0604@hotmail.com>
2025-08-26 09:21:15 +0000

    variables, constants and basic keywords.

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 6677
+let accountemail = "Abc34@gmail.com"
+var accountPassword = "5545"
+accounName = "Abc"
+let accountState;  // undefined variable
+
+// value of a const is can't be changed
+// accountId = 6654 
+console.log("accountID : ", accountId)
+console.table([accountId,accountemail,accountPassword])
+
+/*
+    Preffered Not to use var beacuse of issue in log scope and functional scope
+*/
